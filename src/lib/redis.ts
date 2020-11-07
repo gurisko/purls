4a4cabdf4349143f5d9d81ed3901990e31405106
Matus Gura <matus.gura@gmail.com>
2020-11-07 04:24:56 +0100

    Add redix expiry

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,6 +2,7 @@ import Redis from 'ioredis';
 
 import {castArray} from '../utils/castArray';
 
+const REDIS_EXPIRY = 3600;
 const REDIS_PREFIX = 'purls';
 
 const redisClient = new Redis(process.env.REDIS_URL);
@@ -16,23 +17,18 @@ function addPrefix(key: string) {
 
 export function getAsync(key: string): Promise<string | null>;
 export function getAsync(key: string[]): Promise<(string | null)[]>;
-export function getAsync(key: string|string[]): Promise<string | null | (string | null)[]> {
-  const castedKey = castArray(key);
-  return new Promise((resolve, reject) => redisClient.mget(castedKey.map((key) => addPrefix(key)), (err, reply) => {
-    if (err) {
-      return reject(err);
-    }
-    return resolve(Array.isArray(key) ? reply : reply[0]);
-  }));
+export async function getAsync(key: string|string[]): Promise<string | null | (string | null)[]> {
+  const keys = castArray(key);
+  const data = await redisClient
+    .pipeline(keys.map((key) => ['get', addPrefix(key)]))
+    .exec();
+  const results = data.map(([, result]) => result);
+  return results.length === 1 ? results[0] : results;
 }
 
-export function putAsync<T>(key: string, data: T): Promise<T> {
-  return new Promise((resolve, reject) => redisClient.set(addPrefix(key), wrapValue(data), (err) => {
-    if (err) {
-      return reject(err);
-    }
-    return resolve(data);
-  }));
+export async function putAsync<T>(key: string, data: T): Promise<T> {
+  await redisClient.set(addPrefix(key), wrapValue(data), 'EX', REDIS_EXPIRY);
+  return data;
 }
 
 export const client = redisClient;
