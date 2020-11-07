3a9d84a6a8ddad3dd60fad12fe4107319a6820f4
Matus Gura <matus.gura@gmail.com>
2020-11-07 04:23:35 +0100

    Add prefix for redis keys

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,17 +2,23 @@ import Redis from 'ioredis';
 
 import {castArray} from '../utils/castArray';
 
+const REDIS_PREFIX = 'purls';
+
 const redisClient = new Redis(process.env.REDIS_URL);
 
 function wrapValue(value: any): string {
   return typeof value === 'string' ? value : JSON.stringify(value);
 }
 
+function addPrefix(key: string) {
+  return `${REDIS_PREFIX}:${key}`;
+}
+
 export function getAsync(key: string): Promise<string | null>;
 export function getAsync(key: string[]): Promise<(string | null)[]>;
 export function getAsync(key: string|string[]): Promise<string | null | (string | null)[]> {
   const castedKey = castArray(key);
-  return new Promise((resolve, reject) => redisClient.mget(castedKey, (err, reply) => {
+  return new Promise((resolve, reject) => redisClient.mget(castedKey.map((key) => addPrefix(key)), (err, reply) => {
     if (err) {
       return reject(err);
     }
@@ -21,7 +27,7 @@ export function getAsync(key: string|string[]): Promise<string | null | (string 
 }
 
 export function putAsync<T>(key: string, data: T): Promise<T> {
-  return new Promise((resolve, reject) => redisClient.set(key, wrapValue(data), (err) => {
+  return new Promise((resolve, reject) => redisClient.set(addPrefix(key), wrapValue(data), (err) => {
     if (err) {
       return reject(err);
     }
